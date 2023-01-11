f9edf2d0b98c211fa6914f248c7997cb8e68d442
tilak1988 <31179974+tilak1988@users.noreply.github.com>
2023-01-11 16:07:14 +0530

    Add files via upload

---
+++ auto_scaling/varibles.tf
@@ -0,0 +1,27 @@
+variable "vpc_id" {
+
+}
+
+variable "target_group_arn" {
+
+}
+
+variable "subnet1" {
+
+}
+
+variable "subnet2" {
+
+}
+
+variable "ami_id" {
+    type = string
+}
+
+variable "instance_type" {
+    type = string
+}
+
+variable "key_id" {
+    type = string  
+}
